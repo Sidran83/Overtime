20689879896f4a6b88e6e949519255afa4fb10c6
pierre deveix <deveixpierre@gmail.com>
2021-05-19 14:17:44 +0200

    added test for post model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe User, type: :model do
       @user = User.create(email: 'test@test.com', password: 'azeaze', password_confirmation: 'azeaze', first_name: 'Jon', last_name: 'Snow')
     end
 
-    it 'can be valid' do
+    it 'can be created' do
       expect(@user).to be_valid
     end
 
-    it 'cannot be valid' do
+    it 'cannot be created without first_name, last_name' do
       @user.first_name = nil
       @user.last_name = nil
-      expect(@user).not_to be_valid
+
+      expect(@user).to_not be_valid
     end
   end
 end
