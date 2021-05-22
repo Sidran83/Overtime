e08858eacd43ba2d158120ef9b772b14194e39c3
pierre deveix <deveixpierre@gmail.com>
2021-05-22 14:49:47 +0200

    fullname method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: 'test@test.com', password: 'azeaze', password_confirmation: 'azeaze', first_name: 'Jon', last_name: 'Snow')
+
+puts 'One User was created'
 20.times do |post|
-  Post.create!(date: Date.today, rationale: "post #{post} rationale content")
+  Post.create!(date: Date.today, rationale: "post #{post} rationale content", user_id: @user.id)
 end
 
-puts "20 posts have been created"
+puts '20 posts have been created'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'creation' do
-    before do
-      @user = User.create(email: 'test@test.com', password: 'azeaze', password_confirmation: 'azeaze', first_name: 'Jon', last_name: 'Snow')
-    end
+  before do
+    @user = User.create(email: 'test@test.com', password: 'azeaze', password_confirmation: 'azeaze', first_name: 'Jon', last_name: 'Snow')
+  end
 
+  describe 'creation' do
     it 'can be created' do
       expect(@user).to be_valid
     end
@@ -15,6 +15,12 @@ RSpec.describe User, type: :model do
       @user.last_name = nil
 
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe 'custom name method' do
+    it 'has a fullname method that combined first and last name' do
+      expect(@user.full_name).to eq('SNOW, JON')
     end
   end
 end
